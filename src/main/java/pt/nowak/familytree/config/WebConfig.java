fbc5c7f5ac48e6c0c4bb6e5c3dfeadb9bfc8946f
Patryk Nowak <nowak.p92@gmail.com>
2017-01-16 02:39:48 +0100

    added servlet configuration

---
+++ src/main/java/pt/nowak/familytree/config/WebConfig.java
@@ -0,0 +1,18 @@
+package pt.nowak.familytree.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pt.nowak.familytree")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
